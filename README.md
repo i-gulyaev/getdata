a9fbfa4f65f8065a8b8588d4de75a347cf732029
Igor Guliaev <igor.s.gulyaev@gmail.com>
2015-07-25 20:31:08 +0300

    Implemented functions to create tidy dataset from UCI HAR Dataset train and test sets

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # getdata
-Course Project for "Getting and Cleaning Data" course from coursera.org
+
+This repository contains script *run_analisys.R* which combines training
+and test sets of UCI HAR Dataset (*https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip*)
+to one dataset which contains records of activity type and measurements for each subject.
+Resulting dataset is written to *tidy_dataset.txt* file in the working directory.
+
+
+## Requirements
+- script uses dplyr library v0.4. Make sure it is installed
+- script assumes the dataset is in *UCI HAR Dataset* directory in working directory. So, data shall be downloaded and unpacked before script run
+
+## Examples
+*R < download_dataset.R*
+*R < run_analisys.R --save*
--- run_analysis.R
+++ run_analysis.R
@@ -1,42 +1,93 @@
+## This mofule contains a set of ruoutinse to transform createTidyDataset
+## to one tidy dataset which contains records of activity and activity
+## measurements for each subject
 
 library(dplyr)
 
-
-getFeatures <- function(featurefile) {
-    features <- tbl_df(read.table(featurefile, col.names=c("id", "name")))
+## Function loads feature names from given file and
+## selects only those features wich correspnds to
+## mean and standard deviation of measurement.
+## Return value: dataframe with feature id and feature name columns
+getFeatureNames <- function(filename) {
+    features <- tbl_df(read.table(filename, col.names=c("id", "name")))
     features <- filter(features, grepl("(\\-mean\\()|(\\-std\\()", name))
     features
 }
 
+## Function loads activity labels from given file.
+## Return value: dataframe with label_id and activity columns
+getActivityNames <- function(filename) {
+    activity <- tbl_df(read.table(filename,
+                                  col.names=c("label_id", "activity")))
+    activity
+}
 
-getLabels <- function(labelfile, activityfile) {
-    labels <- tbl_df(read.table(labelfile, col.names=c("label")))
-    activity <- tbl_df(read.table(activityfile, col.names=c("label", "name")))
+## Function loads loabels from given file and assigns
+## each label_id the activity name provided via activity dataframe
+## returned by getActivityName function.
+## Return value: dataframe with label_id and activity columns
+getLabels <- function(filename, activity) {
+    labels <- tbl_df(read.table(filename, col.names=c("label_id")))
     left_join(labels, activity)
 }
 
-createDataset <- function(datafile, features , subjectfile, labels) {
-    
-    measurements <- tbl_df(read.table(datafile))
-
+## Function loads measurements from given file,  filters out
+## those values which do not correspond to desired features
+## provided as second parameter, assigns descriptive names
+## to each feature.
+## Return value: dataframe with columns correspond to features$name vector
+getMeasurements <- function(filename, features) {
+    measurements <- tbl_df(read.table(filename))
     measurements <- select(measurements, features$id)
     names(measurements) <- features$name
+    measurements
+}
 
-    subject <- tbl_df(read.table(subjectfile, col.names=c("subject")))
-    
-    dataset <- bind_cols(subject, labels, measurements)
-
-
-    dataset
-    ## set names to dataset from features
-    
-    ## select apropriate columns with mean in std suffix
-    
-    ## combine datafile, subjectfile and labels
-
+## Function loads subject identifiers from given file
+## Reutrn value: dataframe with subject column
+getSubjects <- function(filename) {
+    subjects <- tbl_df(read.table(filename, col.names=c("subject")))
+    subjects
 }
 
 
-features <- getFeatures("UCI HAR Dataset/features.txt")
-labels <- getLabels("UCI HAR Dataset/test/y_test.txt", "UCI HAR Dataset/activity_labels.txt")
-ds <- createDataset("UCI HAR Dataset/test/X_test.txt", features, "UCI HAR Dataset/test/subject_test.txt", labels)
+## Function creates dataset of subjects, labels and measurements from given files
+## Return value: dataframe combined from results of getSubjects(), getLabels()
+## and getMeasurements(). It contains records of activity type and measurements
+## for each subject 
+loadDataset <- function(m_file, l_file, s_file, features, activity) {
+    labels <- getLabels(l_file, activity)
+    measurements <- getMeasurements(m_file, features)
+    subjects <- getSubjects(s_file)
+    dataset <- bind_cols(subjects, labels, measurements)
+    dataset
+}
+
+## Function creates tidy dataset by combining train and test data of
+## UCI HAR Dataset files and writes the result to file tidy_dataset.txt
+createTidyDataset <- function() {
+    features <- getFeatureNames("UCI HAR Dataset/features.txt")
+    activity <- getActivityNames("UCI HAR Dataset/activity_labels.txt")
+
+    testSet <- loadDataset("UCI HAR Dataset/test/X_test.txt",
+                           "UCI HAR Dataset/test/y_test.txt",
+                           "UCI HAR Dataset/test/subject_test.txt",
+                           features,
+                           activity)
+
+    trainSet  <- loadDataset("UCI HAR Dataset/train/X_train.txt",
+                             "UCI HAR Dataset/train/y_train.txt",
+                             "UCI HAR Dataset/train/subject_train.txt",
+                             features,
+                             activity)
+
+    fullSet <- bind_rows(testSet, trainSet)
+    tidyDataset <- fullSet %>%
+        select(-label_id) %>%
+        group_by(subject, activity) %>%
+        summarise_each(funs(mean))
+
+    write.table(tidyDataset, file="tidy_dataset.txt", row.names=FALSE)
+}
+
+createTidyDataset()
